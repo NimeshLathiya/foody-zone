0e4abc7afbe6562d21ea27b7a4093cbbdd885a30
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-05-13 07:34:47 +0530

    Update index.js

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,7 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", { value:  });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const cors_1 = __importDefault(require("cors"));
