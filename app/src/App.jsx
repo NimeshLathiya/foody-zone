c528d1d106cfbda66533e892877b4c74dba1cbbf
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-08-17 07:54:36 +0530

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "";
 import styled from "styled-components";
 import SearchResult from "./components/SearchResults/SearchResult";
 
