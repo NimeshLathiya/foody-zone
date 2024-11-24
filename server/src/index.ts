8088b4cedd7d1e810e52064cf8c226016f50a997
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-11-24 10:11:53 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import path from "path";
 import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 
